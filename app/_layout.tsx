f55cc2959e4155abec6fa849197492c9f986f026
sanphetsongjindasak <sanphetsongjindasak@gmail.com>
2025-07-15 21:43:14 +0700

    ci/cd : add workflow to automatically deploy to dev environment server

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -8,13 +12,9 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/shared/hooks/useColorScheme';
 
-export {
-
-  ErrorBoundary,
-} from 'expo-router';
+export { ErrorBoundary } from 'expo-router';
 
 export const unstable_settings = {
-
   initialRouteName: '(tabs)',
 };
 
@@ -27,19 +27,16 @@ export default function RootLayout() {
     CustomFont: require('./assets/fonts/InterTight-Black.ttf'),
     'InterTight-SemiBold': require('./assets/fonts/InterTight-SemiBold.ttf'),
     'InterTight-Regular': require('./assets/fonts/InterTight-Regular.ttf'),
-'LineSeedSans': require('./assets/fonts/LINESeedSans_A_Rg.ttf'),
-'LineSeedSansTH': require('./assets/fonts/LINESeedSansTH_A_Rg.ttf'),
-'LineSeedSansTH_A_XBd':require('./assets/fonts/LINESeedSansTH_A_XBd.ttf'),
-'LineSeedSansTH_A_Bd':require('./assets/fonts/LINESeedSansTH_A_Bd.ttf'),
-'LineSeedSansTH_A_Th':require('./assets/fonts/LINESeedSansTH_A_Th.ttf'),
-'LineSeedSansTH_A_He':require('./assets/fonts/LINESeedSansTH_A_He.ttf'),
-
-
+    LineSeedSans: require('./assets/fonts/LINESeedSans_A_Rg.ttf'),
+    LineSeedSansTH: require('./assets/fonts/LINESeedSansTH_A_Rg.ttf'),
+    LineSeedSansTH_A_XBd: require('./assets/fonts/LINESeedSansTH_A_XBd.ttf'),
+    LineSeedSansTH_A_Bd: require('./assets/fonts/LINESeedSansTH_A_Bd.ttf'),
+    LineSeedSansTH_A_Th: require('./assets/fonts/LINESeedSansTH_A_Th.ttf'),
+    LineSeedSansTH_A_He: require('./assets/fonts/LINESeedSansTH_A_XBd.ttf'),
 
     // FontAwesome icons
     ...FontAwesome.font,
   });
-
 
   useEffect(() => {
     if (error) throw error;
